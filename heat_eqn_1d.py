78b7a5d48c31dadb39a0bd38e497aa8d19ae6dd4
Umar Ahmed <wspuma@gmail.com>
2021-01-25 01:09:36 -0800

    Update heat_eqn_1d.py

--- heat_eqn_1d.py
+++ heat_eqn_1d.py
@@ -1,11 +1,11 @@
 from matplotlib import pyplot as plt
 from matplotlib import animation
-
 import math
 
 
 DELTA_X = 0.5
 DELTA_T = 0.05
+
 
 def gen_range(start, end, step):
     result = []
@@ -13,14 +13,17 @@ def gen_range(start, end, step):
         result.append(start)
         start += step
     return result
+
+
 '''
 [a, b] is the domain of interest (spatially)
-[0, t] is the domain of interest temporally
+[0, stop_time] is the domain of interest temporally
 f indicates the initial distribution over [a, b] at t = 0
+we assume the endpoints remain constant
 '''
 
 def solveEquation(a, b, f, stop_time):
-    result = []
+    result = [] # will store the values at each time step so we can animate it later
     
     nodes = gen_range(a, b, DELTA_X)
     initial_vals = [f(x) for x in nodes]
@@ -28,12 +31,12 @@ def solveEquation(a, b, f, stop_time):
 
     cur_time = DELTA_T
     while cur_time <= stop_time:
-
         # Approximate the distribution at t = cur_time
         new_vals = [initial_vals[0]]
         n = len(nodes)
 
         # Update all of the nodes (except the first and last)
+        # We use Euler time stepping here and a 3 point approximation
         for i in range(1, n - 1):
             new_node = initial_vals[i] + DELTA_T * (initial_vals[i + 1] + initial_vals[i - 1] - 2 * initial_vals[i]) / (DELTA_X * DELTA_X)
             new_vals.append(new_node)
@@ -45,8 +48,6 @@ def solveEquation(a, b, f, stop_time):
         cur_time += DELTA_T
         
     return result
-
-
 
 
 
@@ -67,8 +68,4 @@ def animate(i):
     return line,
 
 ani = animation.FuncAnimation(fig, animate, frames = len(result))
-
-
 #ani.save('animation.mp4')
-
-
