d92f0860fc5f0fa666e8e8b4ad0ca77270f4a80d
Umar Ahmed <wspuma@gmail.com>
2021-03-15 16:37:09 -0700

    fixed interp matrix (off by 1 error)

--- cpp/cpm_util.cc
+++ cpp/cpm_util.cc
@@ -57,7 +57,7 @@ int kClosest(const std::vector<double> arr, const double val, const int k = 4) {
             right++;
         }
     }
-    return left;
+    return left + 1;
 }
 
 
@@ -71,6 +71,13 @@ double lagrange1D(const double x, const std::vector<double> arr, const int i) {
         result *= (x - arr[j]) / (arr[i] - arr[j]);
     }
     return result;
+}
+
+
+// Utility printer for debugging
+template <typename T>
+void print(const T x) {
+    std::cout << x << std::endl;
 }
 
 
@@ -94,7 +101,7 @@ sp_mat createInterpMatrix(const std::vector<double> x_pts, const std::vector<dou
             x_stencil[i] = x_pts[x_start + i];
             y_stencil[i] = y_pts[y_start + i];
         }
-        
+
         // Compute and place weight
         for (int i = 0; i < K; i++) {
             for (int j = 0; j < K; j++) {
@@ -115,14 +122,15 @@ sp_mat createInterpMatrix(const std::vector<double> x_pts, const std::vector<dou
 
 
 int main() {
-    // confirmed x_pts, y_pts are good
+    // Create grid
+    constexpr double dx = 0.1;
     std::vector<double> x_pts;
     std::vector<double> y_pts;
     double t = -2;
-    while (t < 2 + 0.1) {
+    while (t < 2 + dx) {
         x_pts.push_back(t);
         y_pts.push_back(t);
-        t += 0.1;
+        t += dx;
     }
 
 
@@ -151,13 +159,7 @@ int main() {
         }
     }
 
-
-    auto E = createInterpMatrix(x_pts, y_pts, cp_pts, band);
-
-
-    
-    //std::cout << E.n_cols() << std::endl;
-    //std::cout << E.n_rows() << std::endl;
-    std::cout << E.row(0) << std::endl;
+    sp_mat laplacian = createLaplacian(band.size(), dx);
+    sp_mat E = createInterpMatrix(x_pts, y_pts, cp_pts, band);
 }
 
